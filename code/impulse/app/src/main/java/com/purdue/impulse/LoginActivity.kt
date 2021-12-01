89e9a09678dc73c38f485bb21575a7e2721b2af6
Azam <mazam1488@gmail.com>
2021-12-01 13:43:01 -0500

    add main activity

--- code/impulse/app/src/main/java/com/purdue/impulse/LoginActivity.kt
+++ code/impulse/app/src/main/java/com/purdue/impulse/LoginActivity.kt
@@ -1,11 +1,18 @@
 package com.purdue.impulse
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        findViewById<Button>(R.id.login_button).setOnClickListener {
+            var intent: Intent = Intent(this, MainActivity::class.java)
+            this.startActivity(intent)
+            finish()
+        }
     }
 }
