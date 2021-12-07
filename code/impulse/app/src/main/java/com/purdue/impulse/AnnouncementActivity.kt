fd8f5535c5ae0d83c83447bf33d3a06f2eb31eee
Azam <mazam1488@gmail.com>
2021-12-07 11:38:24 -0500

    add makeannouncment

--- code/impulse/app/src/main/java/com/purdue/impulse/AnnouncementActivity.kt
+++ code/impulse/app/src/main/java/com/purdue/impulse/AnnouncementActivity.kt
@@ -1,11 +1,37 @@
 package com.purdue.impulse
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog
+import java.util.*
 
-class AnnouncementActivity : AppCompatActivity() {
+class AnnouncementActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener, com.wdullaer.materialdatetimepicker.date.DatePickerDialog.OnDateSetListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_announcement)
+        setClickListeners();
     }
+
+    private fun setClickListeners() {
+        var dateTimeView = findViewById<EditText>(R.id.event_time)
+        dateTimeView.setOnClickListener {
+            val now: Calendar = Calendar.getInstance()
+            val datePickerDialog: DatePickerDialog = DatePickerDialog.newInstance(
+                this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+            )
+            datePickerDialog.show(supportFragmentManager, "DatePickerDialog")
+        }
+    }
+
+    override fun onTimeSet(view: TimePickerDialog?, hourOfDay: Int, minute: Int, second: Int) {
+    }
+
+    override fun onDateSet(view: DatePickerDialog?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+    }
+
 }
